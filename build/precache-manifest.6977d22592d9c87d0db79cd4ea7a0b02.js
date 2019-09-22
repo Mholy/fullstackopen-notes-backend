d161bedb168bc58875293196440bcc703a3fb04f
Eugene Leto <mholy@protonmail.ch>
2019-09-22 17:12:08 +0500

    static build

---
+++ build/precache-manifest.6977d22592d9c87d0db79cd4ea7a0b02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e89fc60b92bf88960cbdc93419bc345a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45316e00c13ab902f8c6",
+    "url": "/static/css/main.65d37e3b.chunk.css"
+  },
+  {
+    "revision": "4de7beea6d3ed897db38",
+    "url": "/static/js/2.d2df994a.chunk.js"
+  },
+  {
+    "revision": "45316e00c13ab902f8c6",
+    "url": "/static/js/main.28e7f341.chunk.js"
+  },
+  {
+    "revision": "fe735450683742ea41fe",
+    "url": "/static/js/runtime~main.659ad52c.js"
+  }
+]);
