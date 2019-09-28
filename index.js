3b191820cc1809c430105f9614a21364c15571e7
Eugene Leto <mholy@protonmail.ch>
2019-09-28 20:09:58 +0500

    module with note model for connecting to mongodb

--- index.js
+++ index.js
@@ -1,32 +1,14 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const mongoose = require('mongoose')
+const Note = require('./models/note')
 
 app.use(express.static('build'))
 app.use(cors())
 app.use(bodyParser.json())
-
-let notes = [
-    {
-        id: 1,
-        content: 'HTML is easy',
-        date: '2019-05-30T17:30:31.098Z',
-        important: true,
-    },
-    {
-        id: 2,
-        content: 'Browser can execute only Javascript',
-        date: '2019-05-30T18:39:34.091Z',
-        important: false,
-    },
-    {
-        id: 3,
-        content: 'GET and POST are the most important methods of HTTP protocol',
-        date: '2019-05-30T19:20:14.298Z',
-        important: true,
-    },
-]
 
 app.get('/api', (req, res) => {
     res.send('<h1>Hello World</h1>')
@@ -89,7 +71,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
