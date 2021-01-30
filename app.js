9283ae0b08ae016c175252dc7b372889f56a049c
Eugene Leto <mholy@protonmail.ch>
2021-01-30 12:32:37 +0500

    5d: controlling state of database for e2e testing

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ const logger = require('./utils/logger')
 logger.info('connecting to', config.MONGODB_URI)
 
 mongoose
-    .connect(config.MONGODB_URI, { useNewUrlParser: true })
-    .then(() => {
-        logger.info('connected to MongoDB')
-    })
-    .catch((error) => {
-        logger.error('error connection to MongoDB:', error.message)
-    })
+  .connect(config.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
 
 app.use(cors())
 app.use(express.static('build'))
@@ -29,6 +29,11 @@ app.use(middleware.requestLogger)
 app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/notes', notesRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
